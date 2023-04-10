ee471e7674ab02ef06573604fd07dbadf847eccd
Thainara Antunes <thainaraantunes@gmail.com>
2023-04-10 14:37:57 -0300

    Add form to add new patients to patient table

--- bmi_table/js/principal.js
+++ bmi_table/js/principal.js
@@ -22,15 +22,49 @@ for(var i=0; i < patients.length; i++){
 		console.log("Wrong weight!")
 		weightOK = false;
 		tdBmi.textContent = "Wrong weight!"
+		patient.classList.add("invalid-patient")
 	}
 
 	if(height < 0 || height > 3.00){
 		console.log("Wrong height!")
 		heightOK = false;
 		tdBmi.textContent = "Wrong height!"
+		patient.classList.add("invalid-patient")
 	}
 
 	if (weightOK && heightOK){
 		tdBmi.textContent = (weight / (height * height)).toFixed(2);
 	}
 } 
+
+var addButton = document.querySelector("#add-patient");
+
+addButton.addEventListener("click", function(){
+	event.preventDefault();
+	var form = document.querySelector("#form-add");
+	// var name = form.name.value;
+	// var weight = form.weight.value;
+	// var height = form.height.value;
+	// var fat = form.fat.value;
+
+	var patientTr = document.createElement("tr");
+	var nameTd = document.createElement("td");
+	var weightTd = document.createElement("td");
+	var heightTd = document.createElement("td");
+	var fatTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nameTd.textContent = form.name.value;
+	weightTd.textContent = form.weight.value;
+	heightTd.textContent = form.height.value;
+	fatTd.textContent = form.fat.value;
+
+	patientTr.appendChild(nameTd);
+	patientTr.appendChild(weightTd);
+	patientTr.appendChild(heightTd);
+	patientTr.appendChild(fatTd);
+
+	var patientTable = document.querySelector("#patientTable");
+	patientTable.appendChild(patientTr);
+
+});
